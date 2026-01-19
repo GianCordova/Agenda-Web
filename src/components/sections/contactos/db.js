ab4c5d07824991eae4b149876366543e332a57bc
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-01-19 13:36:02 -0600

    Se soluciono un error de archivos

--- src/components/sections/contactos/db.js
+++ src/components/sections/contactos/db.js
@@ -1,13 +1,7 @@
-import { getContactsFromStorage } from "./localStorage/storage.js";
-
-let ContactList = getContactsFromStorage();
-
-if (ContactList.length == 0) {
-    ContactList = [
-        { nombre: "Juan", telefono: "123456789"},
-        { nombre: "Giancarlo", telefono: "123456789"},
-        { nombre: "Yovanni", telefono: "123456789"}
-    ];
-}
+let ContactList = [
+    { nombre: "Juan", telefono: "123456789"},
+    { nombre: "Giancarlo", telefono: "123456789"},
+    { nombre: "Yovanni", telefono: "123456789"}
+];
 
 export { ContactList };

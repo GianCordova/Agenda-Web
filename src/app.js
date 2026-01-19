5e2f7f19699b52ba03232dbaedacfd5da9fa9dd3
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-01-19 13:27:58 -0600

    Se hizo la implementacion de LocalStorage para los contactos

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 import { Button } from "./components/common/button/Button.js";
 import { Contactos } from "./components/sections/contactos/Contactos.js";
+import { NuevoContacto } from "./components/sections/nuevoContacto/NuevoContacto.js";
 import { viewContacts, viewNuevoContacto } from "./components/layout/nav/NavControlers.js";
 
-//App
 let app = document.getElementById("app");
 
-//Section menu
 let nav = document.getElementById("nav");
 
 nav.appendChild(Button(
     "Agenda",
     "agenda", 
     "user.svg",
+    viewContacts
 ));
 nav.appendChild(Button(
     "Crear contacto", 
     "plus", 
     "new.svg",
+    viewNuevoContacto
 ));
 nav.appendChild(Button("ToDoList", "todoList", "agenda.svg"));
 nav.appendChild(Button("Crear Tarea", "plus", "new.svg"));
 
-//Section container
 let container = document.getElementById("container");
 
 container.innerHTML = "";
